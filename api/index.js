fabc4f64426486ccd7e152f5824548a19365fcb6
Anup Kumar <anupkumarggupta@gmail.com>
2023-12-13 15:41:11 +0530

    database created and  connected

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
-import express from 'express';
-const app=express();
-const port=3000;
-app.listen(port,()=>{
-    console.log(`server is running on ${port}!`);
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to mongo db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+const port = 3000;
+app.listen(port, () => {
+  console.log(`server is running on ${port}!`);
 });
